787af6d47495e1520e4f56e01c1e19be3e46e855
David Dlp <dlpdavid17@gmail.com>
2022-10-23 12:05:07 +0200

    add images db and inventory

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from flask import Flask, render_template, request, redirect, url_for
 import db
+from models.products import Product
 
 # Inicio servidor Flask
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    print("Iniciamos con una prueba")
-    return render_template("index.html")
+    all_products = db.session.query(Product).all()
+    for item in all_products:
+        print(item)
+    return render_template("index.html", all_products=all_products)
 
 if __name__ == '__main__':
     db.Base.metadata.drop_all(bind=db.engine, checkfirst=True)
 
     db.Base.metadata.create_all(db.engine)
+    Product.addInitialProducts()
 
     app.run(debug=True)
 
--- models/products.py
+++ models/products.py
@@ -1,17 +1,28 @@
-from sqlalchemy import Column, Integer, String
-from db import Base
+from sqlalchemy import Column, Integer, String, BLOB, Numeric, Boolean
+from db import Base, session
+
 
 class Product(Base):
-
-    __tablename__ = 'product'
+    __tablename__ = 'products'
     __table_args__ = {'sqlite_autoincrement': True}
     id_product = Column(Integer, primary_key=True)
     product_name = Column(String(50), nullable=False)
+    mark = Column(String(50))
+    price = Column(Numeric(10, 2))
+    quantity = Column(Integer(), nullable=False)
     description = Column(String(255), nullable=False)
+    stock = Column(Boolean)
+    image = Column(String(30))
 
-    def __init__(self,product_name, description):
+    def __init__(self, product_name, mark, price, quantity, description, stock, image="../static/image/image-db/image-default.jpg"):
         self.product_name = product_name
+        self.mark = mark
+        self.price = price
+        self.quantity = quantity
         self.description = description
+        self.stock = stock
+        self.image = image
+
 
     def __repr__(self):
         return f"Product: {self.id_product} --> {self.product_name}, {self.description}"
@@ -19,3 +30,13 @@ class Product(Base):
     def __str__(self):
         return f"Product: {self.id_product} --> {self.product_name}, {self.description}"
 
+    def addInitialProducts():
+        p1 = Product("teclado", "nisu", 20.5, 10, "Teclado común de marca desconocida", True, "../static/image/image-db/keyboard.png" )
+        p2 = Product("raton", "nisu", 12, 10, "Raton común de marca desconocida", True)
+        p3 = Product("portatil", "MSI", 1200.25, 10, "Portatil i7 16GB de RAM 15.6 pulgadas", True,  "../static/image/image-db/laptop.jpg")
+        p4 = Product("camara web", "Trush", 30.5, 10, "camara HD 1080P con microfono incorporado", True)
+        p5 = Product("iPhone 13", "Apple", 990, 10, "movil de ultima geeneración", True, "../static/image/image-db/smartphone-iphone.png")
+        p6 = Product("teclado & raton inalambricos", "Trush", 45.25, 10, "Teclado y raton común inalambricos ", True, "../static/image/image-db/mouse-keyboard.png")
+        session.add_all([p1, p2, p3, p4, p5, p6])
+        session.commit()
+        session.close()
