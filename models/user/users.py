8d50ff1df3e9332359919076ac5254cb16f184cd
David Dlp <dlpdavid17@gmail.com>
2022-11-02 11:57:18 +0100

    create funcionality for login and register

--- models/user/users.py
+++ models/user/users.py
@@ -1,20 +1,19 @@
-
-from werkzeug.security import check_password_hash
+from flask_login import UserMixin
+# database
 from sqlalchemy import Column, Integer, String, ForeignKey
-from db import Base, session
+from db import Base, db_session
 
 
-class User(Base):
+class User(Base, UserMixin):
     __tablename__ = 'users'
     __table_args__ = {'sqlite_autoincrement': True}
-    id_user = Column(Integer, primary_key=True)
-    username = Column(String(50), unique=True,  index=True)
-    password = Column(String(150), unique=True)
+    id = Column(Integer, primary_key=True)
+    username = Column(String(20), nullable=False, unique=True, index=True)
+    password = Column(String(100), nullable=False)
     name = Column(String(50))
     role_id = Column(Integer, ForeignKey("roles.id_role"))
 
-
-    def __init__(self, username, password, name ="", role_id=2):
+    def __init__(self, username, password, name=None, role_id=2):
         self.username = username
         self.password = password
         self.name = name
@@ -26,18 +25,19 @@ class User(Base):
     def __str__(self):
         return f"User: {self.username} --> {self.name}"
 
-    @classmethod
-    def check_password(self, hash_password, password):
-        return check_password_hash(hash_password, password)
-
 def addAdmin():
-        user_admin = User("Administrador", "pbkdf2:sha256:260000$cfzlwvOJL2VXhluv$21eafd6e64c50a40fb2330648eafce154502c57dacf1c86d0a58b936967921e2", "Federico", role_id=1)
-        session.add(user_admin)
-        session.commit()
-        session.close()
+    user_admin = User("Administrador",
+                      "$2b$12$tQPomBKPzxblsY0Pncox6uiBZ8kN92usHRtM/bitHvO0zpEKgeVL6",
+                      "Federico", role_id=1)
+    db_session.add(user_admin)
+    db_session.commit()
+    db_session.close()
+
 
 def addInitialUser():
-        user_test = User("Testeando", "pbkdf2:sha256:260000$Uuz7LjONYMg4c3Uw$78fe679ad8b437e73bbe05851899357df421d537e9e3c3890de9b652568c0316", "Dexter")
-        session.add(user_test)
-        session.commit()
-        session.close()
+    user_test = User("Testeando",
+                     "$2b$12$6Soo91Akh3WY/bcbj8iG/O0nxviDVnFmHoo5rXewd7drKZoihm.Re",
+                     "Dexter")
+    db_session.add(user_test)
+    db_session.commit()
+    db_session.close()
