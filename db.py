fa6f89c059e71547c2d73ee63952ce9c3e1897cb
David Dlp <dlpdavid17@gmail.com>
2022-11-02 11:50:19 +0100

    change config for login user

--- db.py
+++ db.py
@@ -1,12 +1,14 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base, relationship
+from sqlalchemy.orm import sessionmaker, declarative_base, relationship, scoped_session
 
 # Engine
 engine = create_engine('sqlite:///database/almacen_informatico.db', connect_args={'check_same_thread': False})
 
 # Session
-Session = sessionmaker(bind=engine)
-session = Session()
+db_session = scoped_session(sessionmaker(autocommit=False,
+                       autoflush=False,
+                       bind=engine))
 
 Relation = relationship
 Base = declarative_base()
+Base.query = db_session.query_property()
