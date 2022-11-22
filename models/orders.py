75176d4211d3c38d4775fdb7315fa578a96db504
David Dlp <dlpdavid17@gmail.com>
2022-11-22 06:59:27 +0100

    error corrected

--- models/orders.py
+++ models/orders.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, Integer, ForeignKey
-from db import Base, Relation
+from db import Base
 
 class Order(Base):
     __tablename__ = 'orders'
@@ -8,8 +8,12 @@ class Order(Base):
     product_id = Column(Integer, ForeignKey("products.id_product"))
     user_id = Column(Integer, ForeignKey("users.id"))
 
+    def __init__(self, product_id, user_id):
+        self.product_id = product_id
+        self.user_id = user_id
+
     def __repr__(self):
-        return f"Order: {self.order.id} --> {self.user_id} --> {self.product_id}"
+        return f"Order: {self.order_id} --> {self.user_id} --> {self.product_id}"
 
     def __str__(self):
-        return f"Order: {self.order.id} --> {self.user_id} --> {self.product_id}"
+        return f"Order: {self.order_id} --> {self.user_id} --> {self.product_id}"
