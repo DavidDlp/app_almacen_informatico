9fce150e07785dce886a0590d4d7a6f95f796bee
David Dlp <dlpdavid17@gmail.com>
2022-11-22 07:03:27 +0100

    create funcionality create orders

--- run.py
+++ run.py
@@ -1,16 +1,16 @@
 from flask import Flask, render_template, redirect, url_for, flash
 from flask_bcrypt import Bcrypt
-from flask_login import login_user, LoginManager, login_required, logout_user
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
 
 # DataBase
 from db import Base, engine, db_session
 # Form
 from forms.formUsers import RegisterFormUser, LoginForm
 # Models
-from models.products import Product
+from models.products import Product, addInitialProducts
 from models.roles import addRole
-# Models user
 from models.users import User, addInitialUser, addAdmin, addInitialSupplier
+from models.orders import Order
 
 
 # Inicio servidor Flask
@@ -95,14 +95,28 @@ def register_supplier():
 def create_new_product():
     return render_template('product/create-product.html')
 
-
-
+@app.route('/create-order/<id>', methods=['GET', 'POST'])
+def create_order(id):
+    product_id = id
+    # print(product_id)
+    product = db_session.query(Product).filter(Product.id_product == product_id).first()
+    product.quantity -= 1
+    user_id = current_user.id
+    # print(user_id)
+    order = Order(product_id, user_id)
+    db_session.add(order)
+    db_session.commit()
+    db_session.close()
+    print("Cantidad modificada con exito")
+    print("Orden de compra CREADA")
+    flash("Compra realizada con exito")
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     Base.metadata.drop_all(bind=engine, checkfirst=True)
 
     Base.metadata.create_all(engine)
-    Product.addInitialProducts()
+    addInitialProducts()
     addRole()
     addAdmin()
     addInitialUser()
